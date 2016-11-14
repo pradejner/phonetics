b8b0714e1f668c84febcf446044d91d6aa538670
Prad Ejner <prad.ejner@gmail.com>
2016-11-14 06:17:23 -0500

    update logger

--- app/src/main/java/com/prad/ejner/phonetics/PhoneticsActivity.java
+++ app/src/main/java/com/prad/ejner/phonetics/PhoneticsActivity.java
@@ -20,7 +20,7 @@ public class PhoneticsActivity extends Activity {
 
         setContentView(phonetics_setup);
 
-        Log.i(TAG, "PhoneticsActivity setCreate() finished.");
+        Log.i(TAG, "PhoneticsActivity onCreate() finished.");
     }
 
     @Override
